77440aaac8cd69af7d958fc6a54baa8b899de80c
j-irion <juliusirion@gmx.de>
2022-07-11 17:36:31 +0200

    Add button objects, EventListenersa, Reformat code
    Reformated the code using the VSCode extension Prettier.

--- script.js
+++ script.js
@@ -1,73 +1,93 @@
+let btnRock = document.getElementById("btn-rock");
+let btnPaper = document.getElementById("btn-paper");
+let btnScissors = document.getElementById("btn-scissors");
+
+btnRock.addEventListener("click", function (e) {
+  console.log(e);
+});
+
+btnPaper.addEventListener("click", function (e) {
+  console.log(e);
+});
+
+btnScissors.addEventListener("click", function (e) {
+  console.log(e);
+});
+
 function computerPlay() {
-    let randomNumber = Math.floor(Math.random() * (3 - 1 +1)) + 1;
-    console.log(randomNumber);
-    
-    switch(randomNumber) {
-        case 1: return "Rock";
-        case 2: return "Paper";
-        case 3: return "Scissors";
-    }
+  let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  console.log(randomNumber);
+
+  switch (randomNumber) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerInput = playerSelection.toLowerCase();
-    let computerInput = computerSelection.toLowerCase();
+  let playerInput = playerSelection.toLowerCase();
+  let computerInput = computerSelection.toLowerCase();
 
-    if (playerInput == computerInput) return 2;
-    if ((playerInput == "rock" && computerInput == "scissors") 
-        || (playerInput == "paper" && computerInput == "rock")
-        || (playerInput == "scissors" && computerInput == "paper")) {
-            return 0;
-        }
-    else return 1;
+  if (playerInput == computerInput) return 2;
+  if (
+    (playerInput == "rock" && computerInput == "scissors") ||
+    (playerInput == "paper" && computerInput == "rock") ||
+    (playerInput == "scissors" && computerInput == "paper")
+  ) {
+    return 0;
+  } else return 1;
 }
 
 function game() {
-    let playerCount = 0;
-    let computerCount = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerInput;
-        while (true) {
-            promptPlayerInput = prompt("Rock, Paper or Scissors");
-            if (inputCorrect(promptPlayerInput)) {
-                playerInput = promptPlayerInput;
-                break;
-            }
-            console.log("Wrong input! Try again")
-        }
-        let computerInput = computerPlay();
-        let winner = playRound(playerInput, computerInput);
-        if (winner == 0) {
-            console.log(`You Win! ${playerInput} beats ${computerInput}`);
-            ++playerCount;
-            continue;
-        }
-        else if (winner == 1) {
-            console.log(`You Lose! ${computerInput} beats ${playerInput}`);
-            ++computerCount;
-            continue;
-        }
-        else {
-            console.log(`Draw! ${playerInput} and ${computerInput} are the same`);
-            continue;
-        }
+  let playerCount = 0;
+  let computerCount = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerInput;
+    while (true) {
+      promptPlayerInput = prompt("Rock, Paper or Scissors");
+      if (inputCorrect(promptPlayerInput)) {
+        playerInput = promptPlayerInput;
+        break;
+      }
+      console.log("Wrong input! Try again");
     }
-    if (playerCount > computerCount) {
-        console.log("You win the game!")
-        return;
+    let computerInput = computerPlay();
+    let winner = playRound(playerInput, computerInput);
+    if (winner == 0) {
+      console.log(`You Win! ${playerInput} beats ${computerInput}`);
+      ++playerCount;
+      continue;
+    } else if (winner == 1) {
+      console.log(`You Lose! ${computerInput} beats ${playerInput}`);
+      ++computerCount;
+      continue;
+    } else {
+      console.log(`Draw! ${playerInput} and ${computerInput} are the same`);
+      continue;
     }
-    if (playerCount < computerCount) {
-        console.log("You lose the game!")
-        return;
-    }
-    console.log("It's a draw!")
+  }
+  if (playerCount > computerCount) {
+    console.log("You win the game!");
+    return;
+  }
+  if (playerCount < computerCount) {
+    console.log("You lose the game!");
+    return;
+  }
+  console.log("It's a draw!");
 }
 
 function inputCorrect(input) {
-    if (input.toLowerCase() == "rock" 
-        || input.toLowerCase() == "paper"
-        || input.toLowerCase() == "scissors") {
-        return true;
-    }
-    return false;
+  if (
+    input.toLowerCase() == "rock" ||
+    input.toLowerCase() == "paper" ||
+    input.toLowerCase() == "scissors"
+  ) {
+    return true;
+  }
+  return false;
 }
